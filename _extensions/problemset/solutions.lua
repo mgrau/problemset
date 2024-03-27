a322b4b39b62f1b81edd125cd6909e8c17141743
Matt Grau <matt.grau@gmail.com>
2024-03-27 09:44:17 -0400

    modify solution block to not conflict with quarto's crossref filter

--- _extensions/problemset/solutions.lua
+++ _extensions/problemset/solutions.lua
@@ -1,21 +1,18 @@
 local show_solutions = false
 
 local function parseMeta(meta)
-  if quarto.doc.is_format("latex") then
-    quarto.doc.use_latex_package("tcolorbox", "skins,breakable")
-  end
   show_solutions = meta['solutions']
 end
 
-local function parseSolutions(div)
-  if div.classes:includes("solution") then
+local function parseSolutions(el)
+  if el.classes:includes('sol') then
     if show_solutions then
       return quarto.Callout({
         type = 'note',
         title = 'Solution',
-        content = div.content,
+        content = el.content,
         icon = "false",
-        collapse = false
+        collapse = true
       })
     else
       return pandoc.Str('')
